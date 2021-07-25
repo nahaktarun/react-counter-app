7c9ed0ecc03209aeccde3d5a3bbb8a2f98e8e0ca
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-07-26 00:35:51 +0530

    added function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  //state = {  }
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+  };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello world</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
