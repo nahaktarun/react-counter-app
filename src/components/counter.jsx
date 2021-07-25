b5dc06329afb3d5fa48cb9315175ef88270f5935
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-07-26 00:46:09 +0530

    setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,8 +8,9 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* //<img src={this.state.imageUrl}></img> */}
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
