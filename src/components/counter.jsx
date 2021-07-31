ddbb50ae09c5f3c0113da5d5c2e8b2123f7fe0a8
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-07-31 21:17:14 +0530

    binding the event handlers with constructor or using the arrow function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,17 +6,45 @@ class Counter extends Component {
     imageUrl: "https://picsum.photos/200",
     tags: ["tag1", "tag2", "tag3"],
   };
+  // randerTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+  handleIncrement = () => {
+    //use arrow function to solve the consturctor to bind event handlers
+    console.log("Increment Clicked", this);
+    //obj.method()
+    //function()
+  };
   render() {
     return (
       <React.Fragment>
         {/* //<img src={this.state.imageUrl}></img> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
           ))}
         </ul>
+        {/* {this.renderTags()} */}
       </React.Fragment>
     );
   }
